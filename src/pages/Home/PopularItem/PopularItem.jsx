b11063b0ef2aa491a816613ac4354c3003ffc43d
Mahadi90 <mahadimhs787@gmail.com>
2023-06-05 00:19:19 +0600

    header footer banner category

---
+++ src/pages/Home/PopularItem/PopularItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle';
+
+const PopularItem = () => {
+    return (
+        <div>
+            <SectionTitle 
+            subHeading={'---Check it out---'}
+            Heading={'FROM OUR MENU'}
+            ></SectionTitle>
+        </div>
+    );
+};
+
+export default PopularItem;
