715ebd9de3fe080ea969a34027db64ea5c353411
Mahadi Hasan <mahadimhs787@gmail.com>
2024-01-18 22:58:37 +0600

    initial commit and header footer added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h2 className='text-red-500 text-5xl text-center'>Bistro Boss Restourant</h2>
+    </>
+  )
+}
+
+export default App
